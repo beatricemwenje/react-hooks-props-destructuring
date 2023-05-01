57a434b25397536fba530f64075499959271abdb
beatrice <beatrice.mwenje@student.moringaschool>
2023-05-01 20:56:59 +0300

    Completed lesson

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,20 @@
 import React from "react";
 
+// Without Destructuring
 function MovieCard({
   title,
   posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
   genres,
-}) {
+  posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
+}) 
+  return 
+    <div className="movie-card">
+@@ -14,4 +15,16 @@ function MovieCard(
+  );
+
+
+/* With Destructuring
+function MovieCard({ title, posterSrc, genres }) 
   return (
     <div className="movie-card">
       <img src={posterSrc} alt={title} />
@@ -12,6 +22,7 @@ function MovieCard({
       <small>{genres.join(", ")}</small>
     </div>
   );
-}
 
-export default MovieCard;
+*/
+
+export default MovieCard ;
